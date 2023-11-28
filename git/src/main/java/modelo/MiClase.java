7146771aa44e2bf26d81c59fcaf412f3206d6e01
Andres_Caso <Andres_Caso@DESKTOP-0OHTNVC.localdomain>
2023-11-28 17:23:07 +0100

    interfaces y clases

---
+++ git/src/main/java/modelo/MiClase.java
@@ -0,0 +1,13 @@
+package modelo;
+
+import interfaces.miinterfaz;
+
+public class MiClase implements miinterfaz{
+
+	@Override
+	public int metododevuelveentero() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
