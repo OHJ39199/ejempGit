87521b77ec9bf2d20f763df852d1b73711db8c9d
Andres_Caso <Andres_Caso@DESKTOP-0OHTNVC.localdomain>
2023-12-05 20:37:09 +0100

    ultimo

--- git/src/main/java/principal/principal.java
+++ git/src/main/java/principal/principal.java
@@ -8,6 +8,7 @@ public class principal {
 		System.out.println("version git hub");
 		System.out.println("versioneditado el domingo");
 		System.out.println("muchisimo trbajo");
+		
 		System.out.println("version de un incompetente");
 
 	}
