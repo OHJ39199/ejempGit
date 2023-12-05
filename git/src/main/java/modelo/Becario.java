56c787b910c53efe92a59d3ac556d96767b2ee2c
Andres_Caso <Andres_Caso@DESKTOP-0OHTNVC.localdomain>
2023-12-05 20:14:02 +0100

    primer cambio de la rama becario

---
+++ git/src/main/java/modelo/Becario.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class Becario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("trabajando duro");
+		System.out.println("este macario trabaja mucho");
+	}
+
+}
